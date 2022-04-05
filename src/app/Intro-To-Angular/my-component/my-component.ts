0c43f32be82fff0243a458b9f70c413c88826ba0
Arthur-codeX <daniel.karanja239@gmail.com>
2022-04-05 17:43:13 +0300

    Added  bootsraps package

--- src/app/Intro-To-Angular/my-component/my-component.ts
+++ src/app/Intro-To-Angular/my-component/my-component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-cool-component',
   templateUrl: './my-component.html',
-  styleUrls: ['./my-component.css'],
+  // template: '<p>This is the coolest component</p>',
+  // styles: ['div{color:green}'],
+  styleUrls: [],
 })
 export class MyCoolComponent {}
