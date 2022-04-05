0175b507d32fb59d766a3d5079c7c4dfd4928087
Arthur-codeX <daniel.karanja239@gmail.com>
2022-04-05 17:07:10 +0300

    Nested Components in Intro To Angur Directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MyCoolComponent } from './my-component/my-component';
-import { NealComp } from './Neal-Component/neal-component';
-import { TheSecondComponentComponent } from './the-second-component/the-second-component.component';
+import { MyCoolComponent } from './Intro-To-Angular/my-component/my-component';
+import { NealComp } from './Intro-To-Angular/Neal-Component/neal-component';
+import { TheSecondComponentComponent } from './Intro-To-Angular/the-second-component/the-second-component.component';
 
 @NgModule({
-  declarations: [AppComponent, MyCoolComponent, NealComp, TheSecondComponentComponent],
+  declarations: [
+    AppComponent,
+    MyCoolComponent,
+    NealComp,
+    TheSecondComponentComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
