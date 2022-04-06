7cb3a657ff90ae24d9542024a3d0df9e053de634
Arthur-codeX <daniel.karanja239@gmail.com>
2022-04-06 17:04:33 +0300

    Built in Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MyCoolComponent } from './Intro-To-Angular/my-component/my-component';
 import { NealComp as ThisIsNeal } from './Intro-To-Angular/Neal-Component/neal-component';
@@ -8,6 +8,8 @@ import { TheSecondComponentComponent } from './Intro-To-Angular/the-second-compo
 import TheInterpolationComponet from './Basics/Data-Binding/inter-polation-component/inter-polation-component';
 import { PropertyBindingComponent } from './Basics/Data-Binding/property-binding/property-binding.component';
 import { EventBindingComponent } from './Basics/Data-Binding/event-binding/event-binding.component';
+import { TwoWayDataBindingComponent } from './Basics/Data-Binding/two-way-data-binding/two-way-data-binding.component';
+import { StructuralDirectivesComponent } from './Basics/Directives/Basics/Directives/structural-directives/structural-directives.component';
 
 @NgModule({
   declarations: [
@@ -18,8 +20,10 @@ import { EventBindingComponent } from './Basics/Data-Binding/event-binding/event
     TheInterpolationComponet,
     PropertyBindingComponent,
     EventBindingComponent,
+    TwoWayDataBindingComponent,
+    StructuralDirectivesComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
